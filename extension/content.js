f751022dbc48f95aaa4ff906862dc42754c39c9f
andrewletz <andrewletz@gmail.com>
2018-11-10 00:53:04 -0800

    Content script blurs all recommended videos

---
+++ extension/content.js
@@ -0,0 +1,28 @@
+// callback executed when canvas was found
+function handleRecs(recs) { 
+	var index = 0, length = recs.length;
+	for ( ; index < length; index++) {
+	    recs[index].style.backgroundColor = "#FFFFFF";
+	    recs[index].style.filter = "blur(10px)";
+	} 
+}
+
+// set up the mutation observer
+var observer = new MutationObserver(function (mutations, me) {
+  // `mutations` is an array of mutations that occurred
+  // `me` is the MutationObserver instance
+  var recs = document.querySelectorAll("ytd-compact-video-renderer");
+  if (recs.length > 19) { // if this is greater than 19, all of our recommendations are loaded in
+    handleRecs(recs);
+    me.disconnect(); // stop observing
+    return;
+  }
+});
+
+// start observing
+observer.observe(document, {
+  attributes: true,
+  childList: true,
+  characterData: true,
+  subtree: true
+});
